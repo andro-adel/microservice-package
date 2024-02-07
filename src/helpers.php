c5d652af6214d224e84c28383ea7cb6a924961a6
Kelany21 <kelany98113@gmail.com>
2024-02-07 15:54:46 +0200

    try helper

---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+
+if (! function_exists('testHelperDD')) {
+    /**
+     * test helper DD.
+     * @return string
+     */
+    function testHelperDD(): string
+    {
+        return "hamada";
+    }
+}
